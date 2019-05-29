ada82319aea5f6b6633e609a876adb7d1df57d09
Erik Gassler <erikgassler@live.com>
2019-05-28 23:35:10 -0700

    Added sample project and fixed dependency conflict with Blazor

---
+++ SampleSharedLib/Interfaces/IAPIResponse.cs
@@ -0,0 +1,11 @@
+﻿//using StoicDreams.Catalog;
+
+namespace StoicDreams.Interfaces
+{
+	public interface IAPIResponse
+	{
+		//APIResult Result { get; set; }
+		object Data { get; set; }
+		int StatusCode { get; set; }
+	}
+}
---
+++ WebShared/Catalog/APIResponse.cs
@@ -0,0 +1,11 @@
+﻿using StoicDreams.Interfaces;
+
+namespace StoicDreams.Catalog
+{
+	public class APIResponse : IAPIResponse
+	{
+		public APIResult Result { get; set; } = APIResult.Error;
+		public object Data { get; set; }
+		public int StatusCode { get; set; } = 200;
+	}
+}
